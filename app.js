fcf692cc0beb8756cc94cc6beec107244368ccc2
Dylan Smith <dylantsmith@gmail.com>
2013-03-15 21:34:11 -0500

    add support for less

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 var express = require("express"),
-    path    = require("path"),
-    fs      = require("fs");
+	fs = require("fs"),
+	lessMiddleware = require("less-middleware");
 
 var app = express();
 
@@ -8,7 +8,6 @@ app.configure(function() {
 	app.set("port", 3000);
 	app.set("views", __dirname + "/views");
 	app.engine('html', require('ejs').renderFile);
-	app.use(express.static(path.join(__dirname, "public")));
 	app.use(express.cookieParser("simple-todo"));
 	app.use(express.cookieSession());
 	app.use(express.bodyParser());
@@ -17,6 +16,17 @@ app.configure(function() {
 		require("./controllers/" + controllerName)(app);
 	});
 	app.use(app.router);
+	app.use(lessMiddleware({
+		src: __dirname + "/less",
+		dest: __dirname + "/public/css",
+		// if you're using a different src/dest directory, you MUST
+		// include the prefex, which matches the dest public directory
+		prefix: '/css',
+		// force true recompiles on every request... not the best
+		// for production, but fine in debug while working through changes
+		force: true
+	}));
+	app.use(express.static(__dirname + "/public"));
 });
 
 app.configure("development", function() {
