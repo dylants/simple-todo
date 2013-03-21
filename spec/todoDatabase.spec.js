32810c993e37609991a4d32667ecf62967dfc96e
Dylan Smith <dylantsmith@gmail.com>
2013-03-20 23:05:52 -0500

    jasmine-node is a devDependency, and build out more TodoDatabase tests

--- spec/todoDatabase.spec.js
+++ spec/todoDatabase.spec.js
@@ -1,7 +1,8 @@
-var rewire = require("rewire");
 var TodoDatabase = require("../utilities/todoDatabase");
 
 describe("A TodoDatabase", function() {
+	var userId = "Biff";
+
 	describe("that is newly constructed", function() {
 		var todoDatabase;
 
@@ -9,8 +10,14 @@ describe("A TodoDatabase", function() {
 			todoDatabase = new TodoDatabase();
 		});
 
+		it("should be defined", function() {
+			expect(todoDatabase.todoDB).toBeDefined();
+		});
 		it("should be empty", function() {
 			expect(todoDatabase.todoDB).toEqual({});
+		});
+		it("should return undefined on a get for our user", function() {
+			expect(todoDatabase.getTodos(userId)).toBeUndefined();
 		});
 	});
 });
